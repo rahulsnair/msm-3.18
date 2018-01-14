MotorolaMobilityLLC/kernel-msm

spi: qsd: Only flush BAM if not in active mode or on error
Flushing the BAM buffer is measured to take 1-2 milliseconds. For
high throughput devices, this is unacceptable. With this patch,
the flush will now only occur on an error. In addition, to prevent
this logic change from affecting other drivers, the flush is only
skipped when in active mode.

Change-Id: I33fb56eeff3aa337b0919987e547b1677ffc28b5
Signed-off-by: Greg Meiste <w30289@motorola.com>
Reviewed-on: http://gerrit.mot.com/799459
SLTApproved: Slta Waiver <sltawvr@motorola.com>
SME-Granted: SME Approvals Granted
Reviewed-by: Jason Hrycay <jason.hrycay@motorola.com>
Submit-Approved: Jason Hrycay <jason.hrycay@motorola.com>
Tested-by: Jason Hrycay <jason.hrycay@motorola.com>

drivers/spi/spi_qsd.c
@@ -1422,7 +1422,7 @@ static int msm_spi_process_transfer(struct msm_spi *dd)
 	msm_spi_udelay(dd->xfrs_delay_usec);
 
 transfer_end:
-	if ((dd->mode == SPI_BAM_MODE) && status)
+	if ((dd->mode == SPI_BAM_MODE) && (!dd->pdata->rt_priority || status))
 		msm_spi_bam_flush(dd);
 	msm_spi_dma_unmap_buffers(dd);
 	dd->mode = SPI_MODE_NONE;

rahulsnair/msm-3.18

drivers/spi/spi_qsd.c
@@ -1471,7 +1471,7 @@ static int msm_spi_process_transfer(struct msm_spi *dd)
 	msm_spi_udelay(dd->xfrs_delay_usec);
 
 transfer_end:
-	if ((dd->tx_mode == SPI_BAM_MODE) && status)
+	if ((dd->tx_mode == SPI_BAM_MODE) && (!dd->pdata->rt_priority || status))
 		msm_spi_bam_flush(dd);
 	msm_spi_dma_unmap_buffers(dd);
 	dd->tx_mode = SPI_MODE_NONE;